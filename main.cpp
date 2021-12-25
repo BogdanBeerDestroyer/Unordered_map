ab703abbc326c06a15853cc3c744f7167b05c148
bogdan <bshumilishsky@mail.ru>
2021-12-25 21:22:15 +0300

    tests configured

--- main.cpp
+++ main.cpp
@@ -3,5 +3,6 @@
 #include "unordered_map.h"
 
 int main() {
+    std::cout << "1";
     return 0;
 }
---
+++ tests.cpp
@@ -0,0 +1,10 @@
+//
+// Created by bogdan on 25.12.2021.
+//
+
+
+#include <gtest/gtest.h>
+
+TEST(something, smth) {
+    EXPECT_TRUE(true);
+}
